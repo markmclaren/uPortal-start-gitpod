2e5703c741a7301e9b8f3a8d22236ea7594d25b5
Mark McLaren <mark.mclaren@gmail.com>
2021-11-12 14:16:44 +0000

    Update global properties

--- create-global-properties.sh
+++ create-global-properties.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 cat << EOF > ./portal/global.properties
+hibernate.connection.driver_class=org.hsqldb.jdbc.JDBCDriver
+hibernate.connection.url=jdbc:hsqldb:hsql://localhost:8887/uPortal
+hibernate.connection.username=sa
+hibernate.connection.password=
+hibernate.connection.validationQuery=select 1 from INFORMATION_SCHEMA.SYSTEM_USERS
+hibernate.dialect=org.hibernate.dialect.HSQLDialect
 ## Portal Server
 portal.protocol=https
 portal.server=${PORTAL_URL:8}
